5bad9dc851a748ad8388d2710f972b46a648a093
Caaron <arn_0000@hotmail.com>
2023-04-12 09:57:37 +0800

    second commit

---
+++ app/src/main/java/com/mandalorian/replybot/di/MyAppDependency.kt
@@ -0,0 +1,39 @@
+package com.mandalorian.replybot.di
+
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.mandalorian.replybot.service.AuthService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object MyAppDependency {
+
+    @Provides
+    @Singleton
+    fun getFireStore(): FirebaseFirestore {
+        return Firebase.firestore
+    }
+
+    @Provides
+    @Singleton
+    fun getFirebaseAuth(): FirebaseAuth {
+        return Firebase.auth
+    }
+
+
+    @Provides
+    @Singleton
+    fun getAuthRepository(auth: FirebaseAuth, db: FirebaseFirestore): AuthService {
+        return AuthService(auth, db.collection("users"))
+    }
+
+}
---
+++ app/src/main/java/com/mandalorian/replybot/model/User.kt
@@ -0,0 +1,7 @@
+package com.mandalorian.replybot.model
+
+data class User(
+    val name: String,
+    val email: String,
+    val pass: String,
+)
