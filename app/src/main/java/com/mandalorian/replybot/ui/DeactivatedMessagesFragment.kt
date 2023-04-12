b5a2ecc9b3dbe8000fdfd8d60189b462f98757e0
juju <jcmc888@gmail.com>
2023-04-12 09:56:52 +0800

    added viewPager

--- app/src/main/java/com/mandalorian/replybot/ui/DeactivatedMessagesFragment.kt
+++ app/src/main/java/com/mandalorian/replybot/ui/DeactivatedMessagesFragment.kt
@@ -6,11 +6,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.mandalorian.replybot.R
-import com.mandalorian.replybot.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
-private lateinit var binding: FragmentHomeBinding
-private val activatedFragment = Ac
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [DeactivatedMessagesFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class DeactivatedMessagesFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
     private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +35,7 @@ private val activatedFragment = Ac
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_deactivated_messages, container, false)
     }
 
     companion object {
@@ -36,12 +45,12 @@ private val activatedFragment = Ac
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment DeactivatedMessagesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            DeactivatedMessagesFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/mandalorian/replybot/ui/presentation/activatedMessages/ActivatedMessagesFragment.kt
+++ app/src/main/java/com/mandalorian/replybot/ui/presentation/activatedMessages/ActivatedMessagesFragment.kt
@@ -1,17 +1,26 @@
-package com.mandalorian.replybot.ui
+package com.mandalorian.replybot.ui.presentation.activatedMessages
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.mandalorian.replybot.R
-import com.mandalorian.replybot.databinding.FragmentHomeBinding
+import com.mandalorian.replybot.databinding.FragmentActivatedMessagesBinding
+import com.mandalorian.replybot.ui.presentation.activatedMessages.viewModel.ActivatedMessagesViewModel
+import com.mandalorian.replybot.ui.presentation.base.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class HomeFragment : Fragment() {
-private lateinit var binding: FragmentHomeBinding
-private val activatedFragment = Ac
-    private var param2: String? = null
+@AndroidEntryPoint
+class ActivatedMessagesFragment : BaseFragment<FragmentActivatedMessagesBinding>() {
+    override val viewModel: ActivatedMessagesViewModel by viewModels()
+    override fun getLayoutResource(): Int = R.layout.fragment_activated_messages
+
+    override fun onBindView(view: View, savedInstanceState: Bundle?) {
+        super.onBindView(view, savedInstanceState)
+        binding?.viewModel = viewModel
+        binding?.lifecycleOwner = viewLifecycleOwner
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,7 +35,7 @@ private val activatedFragment = Ac
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        return inflater.inflate(R.layout.fragment_activated_messages, container, false)
     }
 
     companion object {
@@ -36,12 +45,12 @@ private val activatedFragment = Ac
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
+         * @return A new instance of fragment ActivatedMessagesFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
+            ActivatedMessagesFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
