92d449874314060268a9e16dfbd0ea5c7f528edb
Xiang Ze <xchuay@gmail.com>
2023-04-20 09:48:54 +0800

    latest updates

--- app/src/main/java/com/mandalorian/replybot/ui/presentation/authDirectory/viewModel/loginViewModel.kt
+++ app/src/main/java/com/mandalorian/replybot/ui/presentation/authDirectory/viewModel/loginViewModel.kt
@@ -5,12 +5,15 @@ import com.mandalorian.replybot.service.AuthService
 import com.mandalorian.replybot.ui.presentation.base.viewModel.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class loginViewModel @Inject constructor(private val auth: AuthService):BaseViewModel() {
     val loginFinish: MutableSharedFlow<Unit> = MutableSharedFlow()
+    val email: MutableStateFlow<String> = MutableStateFlow("")
+    val pass: MutableStateFlow<String> = MutableStateFlow("")
     fun login(email: String, pass: String) {
         viewModelScope.launch {
             val res = safeApiCall { auth.login(email, pass) }
